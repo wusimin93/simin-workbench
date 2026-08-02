47113a36e304f545d2151d2f01e673dcdf61a623
思敏工作台 <simin@workbuddy.local>
2026-08-02 13:27:45 +0800

    思敏工作台 v3 - 个人管理应用

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE='simin-v3';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
